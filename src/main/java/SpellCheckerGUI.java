c32ce759df2becb7e67dc831e8e9d8693966c714
hdunl <hdunlap936@gmail.com>
2023-12-06 23:17:51 -0500

    - added ehance with AI - need to fix button styling later when not lazy

--- src/main/java/SpellCheckerGUI.java
+++ src/main/java/SpellCheckerGUI.java
@@ -5,10 +5,24 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import org.asynchttpclient.*;
+import java.util.concurrent.CompletableFuture;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class SpellCheckerGUI extends Application {
     private final SpellChecker spellChecker;
@@ -21,6 +35,8 @@ public class SpellCheckerGUI extends Application {
 
     private Stage pathStage;
     private ListView<String> pathListView;
+    private Button infoButton;
+    private CheckBox useTextGearsCheckbox;
 
     public SpellCheckerGUI() {
         spellChecker = new SpellChecker(2);
@@ -56,8 +72,29 @@ public class SpellCheckerGUI extends Application {
         suggestionsTextArea.setEditable(false);
         suggestionsTextArea.setPrefHeight(300);
 
+        // Create a GridPane for layout
+        GridPane layoutGrid = new GridPane();
+        layoutGrid.setHgap(10);
+        layoutGrid.setVgap(10);
+        layoutGrid.setPadding(new Insets(20));
+
+// Checkbox for "Enhance with AI
+        useTextGearsCheckbox = new CheckBox("Enhance with AI");
+
+// "Details" button
+        Button detailsButton = new Button("[Details]");
+        detailsButton.getStyleClass().add("details-button"); // Add CSS class for styling
+        detailsButton.setOnAction(e -> showApiInfoDialog());
+        detailsButton.setStyle("-fx-font-size: 10px;"); // Adjust the font size
+
+// Add "Enhance with AI" checkbox and "Details" button to the GridPane
+        layoutGrid.add(useTextGearsCheckbox, 0, 0); // Adjust the row and column indices as needed
+        layoutGrid.add(detailsButton, 1, 0); // Adjust the row and column indices as needed
+
+// Add the GridPane to your main layout (adjust the row and column indices accordingly)
         CheckBox showPathCheckBox = new CheckBox("Show Search Path");
-        gridPane.add(showPathCheckBox, 0, 6);
+        gridPane.add(showPathCheckBox, 0, 6); // Adjust the row and column indices as needed
+        gridPane.add(layoutGrid, 0, 7); // Adjust the row and column indices as needed
 
         statsLabel = new Label();
 
@@ -75,16 +112,16 @@ public class SpellCheckerGUI extends Application {
         GridPane.setColumnSpan(statsLabel, 3);
         gridPane.add(statsLabel, 0, 5);
 
+        // Event listeners for sliders
         maxDistanceSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-            int maxDistance = newValue.intValue();
-            maxDistanceValueLabel.setText(String.valueOf(maxDistance));
+            maxDistanceValueLabel.setText(String.valueOf(newValue.intValue()));
         });
 
         maxSuggestionsSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-            int maxSuggestions = newValue.intValue();
-            maxSuggestionsValueLabel.setText(String.valueOf(maxSuggestions));
+            maxSuggestionsValueLabel.setText(String.valueOf(newValue.intValue()));
         });
 
+        // Set up the path stage
         pathStage = new Stage();
         pathStage.setTitle("Search Path");
         pathStage.initStyle(StageStyle.UTILITY);
@@ -99,72 +136,48 @@ public class SpellCheckerGUI extends Application {
         Scene pathScene = new Scene(pathLayout, 200, 300);
         pathStage.setScene(pathScene);
 
+        // Button event handler
+        CheckBox finalShowPathCheckBox = showPathCheckBox;
         checkButton.setOnAction(e -> {
-            String wordToCheck = inputWord.getText();
+            String wordToCheck = inputWord.getText().trim();
 
-            // Clear the previous items from the path list view
             pathListView.getItems().clear();
-
-            // Only show the search path dialog if the checkbox is selected
-            if (showPathCheckBox.isSelected()) {
+            if (finalShowPathCheckBox.isSelected()) {
                 pathListView.getItems().addAll(spellChecker.path());
-                pathStage.show(); // Show the dialog only if the checkbox is selected
+                pathStage.show();
             } else {
-                pathStage.hide(); // Optionally hide the dialog if it's not selected
+                pathStage.hide();
             }
 
             if (wordToCheck.isEmpty() || wordToCheck.matches(".*\\d.*")) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Error");
                 alert.setHeaderText(null);
-
-                if (wordToCheck.isEmpty()) {
-                    alert.setContentText("Please enter a word to check.");
-                } else {
-                    alert.setContentText("Please enter a valid word without numbers.");
-                }
-
-
+                alert.setContentText(wordToCheck.isEmpty() ? "Please enter a word to check." : "Please enter a valid word without numbers.");
                 alert.showAndWait();
                 return;
             }
 
             int maxDistance = (int) maxDistanceSlider.getValue();
             int maxSuggestions = (int) maxSuggestionsSlider.getValue();
-
             spellChecker.clearPath();
 
-            // Start measuring time
-            long startTime = System.nanoTime();
-
-            boolean isSpelledCorrectly = spellChecker.checkWord(wordToCheck);
-            List<Map.Entry<String, Double>> suggestions = spellChecker.suggestCorrections(wordToCheck, maxDistance, maxSuggestions);
-
-            // Stop measuring time
-            long endTime = System.nanoTime();
-            double searchTimeMillis = (double) (endTime - startTime) / 1_000_000;
-
-            // Formatting the output
-            DecimalFormat df = new DecimalFormat("#.##");
-            StringBuilder resultText = new StringBuilder();
-
-            if (isSpelledCorrectly) {
-                resultText.append(wordToCheck).append(" is spelled correctly.\n");
+            String resultText;
+            if (useTextGearsCheckbox.isSelected()) {
+                resultText = callTextGearsAPI(wordToCheck);
             } else {
-                resultText.append(wordToCheck).append(" is spelled incorrectly. Did you mean:\n");
-                for (Map.Entry<String, Double> suggestion : suggestions) {
-                    resultText.append(suggestion.getKey())
-                            .append(" (Score: ")
-                            .append(df.format(suggestion.getValue()))
-                            .append(")\n");
-                }
+                long startTime = System.nanoTime();
+                boolean isSpelledCorrectly = spellChecker.checkWord(wordToCheck);
+                List<Map.Entry<String, Double>> suggestions = spellChecker.suggestCorrections(wordToCheck, maxDistance, maxSuggestions);
+                long endTime = System.nanoTime();
+
+                double searchTimeMillis = (double) (endTime - startTime) / 1_000_000;
+                DecimalFormat df = new DecimalFormat("#.##");
+                resultText = formatSuggestions(isSpelledCorrectly, wordToCheck, suggestions);
+                resultText += "\nSearch Time: " + df.format(searchTimeMillis) + " ms";
             }
 
-            // Append the search time to the result text
-            resultText.append("\nSearch Time: ").append(df.format(searchTimeMillis)).append(" ms");
-
-            // Set the text to the suggestions text area
-            suggestionsTextArea.setText(resultText.toString());
+            suggestionsTextArea.setText(resultText);
             updateStats();
         });
 
@@ -182,6 +195,123 @@ public class SpellCheckerGUI extends Application {
 
         statsLabel.setText(statsText);
     }
+
+    // Method to call the TextGears API
+    private String callTextGearsAPI(String text) {
+        AsyncHttpClient client = new DefaultAsyncHttpClient();
+        String apiKey = "16fbc9e08emsh71b08b0f258851fp150171jsnecf065972717"; // Replace with your actual API key
+        String encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8);
+
+        CompletableFuture<String> futureResponse = new CompletableFuture<>();
+
+        client.preparePost("https://textgears-textgears-v1.p.rapidapi.com/spelling")
+                .setHeader("content-type", "application/x-www-form-urlencoded")
+                .setHeader("X-RapidAPI-Key", apiKey)
+                .setHeader("X-RapidAPI-Host", "textgears-textgears-v1.p.rapidapi.com")
+                .setBody("text=" + encodedText)
+                .execute()
+                .toCompletableFuture()
+                .thenAccept(response -> {
+                    // Parse the response and format the suggestions
+                    String suggestions = parseTextGearsResponse(response.getResponseBody());
+                    futureResponse.complete(suggestions);
+                });
+
+        try {
+            return futureResponse.get(); // This will wait until the future is completed
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error: " + e.getMessage();
+        } finally {
+            try {
+                client.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void showApiInfoDialog() {
+        Alert apiInfoAlert = new Alert(Alert.AlertType.INFORMATION);
+        apiInfoAlert.setTitle("API Information");
+        apiInfoAlert.setHeaderText("TextGears API Details");
+        apiInfoAlert.setContentText("https://rapidapi.com/Textgears/api/textgears/");
+
+        apiInfoAlert.showAndWait();
+    }
+
+    private String parseTextGearsResponse(String jsonResponse) {
+        StringBuilder result = new StringBuilder();
+        JsonParser parser = new JsonParser();
+        JsonObject rootObj = parser.parse(jsonResponse).getAsJsonObject();
+
+        if (rootObj.has("status") && rootObj.get("status").getAsBoolean()) {
+            if (rootObj.has("response") && rootObj.getAsJsonObject("response").has("errors")) {
+                JsonArray errors = rootObj.getAsJsonObject("response").getAsJsonArray("errors");
+
+                for (JsonElement errorElement : errors) {
+                    JsonObject errorObj = errorElement.getAsJsonObject();
+                    String badWord = errorObj.get("bad").getAsString();
+                    JsonArray betterOptions = errorObj.get("better").getAsJsonArray();
+
+                    for (JsonElement option : betterOptions) {
+                        String suggestion = option.getAsString();
+                        double similarityScore = calculateSimilarity(badWord, suggestion);
+                        result.append(suggestion).append(", Similarity: ").append(String.format("%.2f%%\n", similarityScore * 100));
+                    }
+                    result.append("\n");
+                }
+            }
+        } else {
+            result.append("No suggestions found or error in API call.");
+        }
+
+        return result.toString();
+    }
+
+    private double calculateSimilarity(String word1, String word2) {
+        int maxLength = Math.max(word1.length(), word2.length());
+        int commonChars = 0;
+
+        for (int i = 0; i < word1.length() && i < word2.length(); i++) {
+            if (word1.charAt(i) == word2.charAt(i)) {
+                commonChars++;
+            }
+        }
+
+        int totalDifferences = maxLength - commonChars;
+        double positionFactor = 1.0;
+
+        // Check for character position differences
+        for (int i = 0; i < Math.min(word1.length(), word2.length()); i++) {
+            if (word1.charAt(i) != word2.charAt(i)) {
+                // Decrease the position factor based on the position of the difference
+                positionFactor -= 0.1 * (1.0 - (double)i / maxLength);
+            }
+        }
+
+        return (double)commonChars / maxLength * positionFactor;
+    }
+
+
+    private String formatSuggestions(boolean isSpelledCorrectly, String word, List<Map.Entry<String, Double>> suggestions) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        StringBuilder resultText = new StringBuilder();
+
+        if (isSpelledCorrectly) {
+            resultText.append(word).append(" is spelled correctly.\n");
+        } else {
+            resultText.append(word).append(" is spelled incorrectly. Did you mean:\n");
+            for (Map.Entry<String, Double> suggestion : suggestions) {
+                resultText.append(suggestion.getKey())
+                        .append(" (Score: ")
+                        .append(df.format(suggestion.getValue()))
+                        .append(")\n");
+            }
+        }
+        return resultText.toString();
+    }
+
 
     public static void main(String[] args) {
         launch(args);
